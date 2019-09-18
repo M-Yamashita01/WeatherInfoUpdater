bdc0c860676538aad011551cc2611fee3fd90833
Masato Yamashita <m.yamashii@gmail.com>
2019-09-19 07:51:57 +0900

    Implememented insert_weather_group method.#3

--- src/db/db_access.rb
+++ src/db/db_access.rb
@@ -1,4 +1,5 @@
 require 'mysql2'
+require 'time'
 
 class DBAccess
   def initialize
@@ -10,5 +11,17 @@ class DBAccess
 
   def execute_query(query)
     return @client.query(query)
+  end
+
+  def insert_weathermap_location(city_id, latitude, longitude, city_name)
+    current_datetime = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    statement = @client.prepare('INSERT INTO weathermap_locations (city_id, latitude, longitude, city_name, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?)')
+    statement.execute(city_id, latitude, longitude, city_name, current_datetime, current_datetime)
+  end
+
+  def insert_weather_group(weather_group_id, weather_icon, weather_main, weather_description)
+    current_datetime = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    @client.prepare('INSERT INTO weather_groups (weather_group_id, weather_icon, weather_main, weather_description, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?)')
+    statement.execute(weather_group_id, weather_icon, weather_main, weather_description, current_datetime, current_datetime)
   end
 end
