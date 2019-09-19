0e32cc4416bffe09b1ff93bb4be1162755ad13ec
Masato Yamashita <m.yamashii@gmail.com>
2019-09-19 21:35:51 +0900

    Added member of weather datas #3

--- src/openWeatherMap/current_weather.rb
+++ src/openWeatherMap/current_weather.rb
@@ -3,4 +3,23 @@ class CurrentWeather
   attr_accessor :latitude
   attr_accessor :city_id
   attr_accessor :city_name
+
+  attr_accessor :weather_group_id
+  attr_accessor :weather_icon
+  attr_accessor :weather_main
+  attr_accessor :weather_description
+
+  attr_accessor :temperature
+  attr_accessor :pressure
+  attr_accessor :humidity
+  attr_accessor :temperature_min
+  attr_accessor :temperature_max
+  attr_accessor :wind_speed
+  attr_accessor :wind_degree
+  attr_accessor :cloudiness
+  attr_accessor :rain_1h
+  attr_accessor :rain_3h
+  attr_accessor :snow_1h
+  attr_accessor :snow_3h
+  attr_accessor :country_code
 end
